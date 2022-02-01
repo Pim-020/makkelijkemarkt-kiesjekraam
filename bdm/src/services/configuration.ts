d733590244f0e459b02868d5cd12526551f0507f
Pim <pim.visser@amsterdam.nl>
2022-02-01 19:12:59 +0100

    Enable optional env setting for KJK api origin

--- bdm/src/services/configuration.ts
+++ bdm/src/services/configuration.ts
@@ -1,5 +1,5 @@
 export default class Configuration {
-    MMARKT_URL: string = '/api/markt';
+    MMARKT_URL: string = `${process.env.REACT_APP_KJK_API_ORIGIN || ''}/api/markt`;
     API_BASE_URL: string = 'https://bewerkdemarkten.tiltshiftapps.nl/api/v1';
     ONLINE: boolean = false;
 }
