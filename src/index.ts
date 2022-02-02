b465ef7a2f4bbe3a2001a9d941139f467ab6d055
Pim <pim.visser@amsterdam.nl>
2022-02-02 15:19:42 +0100

    Apply bodyparser JSON middleware

--- src/index.ts
+++ src/index.ts
@@ -135,6 +135,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 if (process.env.ENABLE_CORS_FOR_ORIGIN) {
     app.use(cors({ origin: process.env.ENABLE_CORS_FOR_ORIGIN }));
