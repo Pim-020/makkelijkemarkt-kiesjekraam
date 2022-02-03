83d9673dcbe6e9e235ff96839713397f803f5a80
Tim Fierens <t.fierens@amsterdam.nl>
2022-02-03 12:24:42 +0100

    only delete rsvp when put is successful

--- src/migrate-rsvp.ts
+++ src/migrate-rsvp.ts
@@ -1,4 +1,4 @@
-import { updateRsvp,getAanmeldingenByOndernemer } from './makkelijkemarkt-api';
+import { updateRsvp } from './makkelijkemarkt-api';
 import { rsvp } from './model/index';
 import { RSVP } from './model/rsvp.model';
 
@@ -9,12 +9,15 @@ rsvp.findAll<RSVP>({
     rsvps => {
         rsvps.forEach(r => {
             console.log(r);
-            updateRsvp(r.marktId, r.marktDate, r.erkenningsNummer, r.attending);
-            rsvp.destroy({
-                where: {
-                    id: r.id
-                }
-            });
+            updateRsvp(r.marktId, r.marktDate, r.erkenningsNummer, r.attending).then( updatedRsvp => {
+                rsvp.destroy({
+                    where: {
+                        id: r.id
+                    }
+                });
+            }).catch( () =>
+                console.log(`${r.id} failed`)
+            )
         })
     }
 );
