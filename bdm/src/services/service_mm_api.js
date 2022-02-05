86817d9e49ad70c1e7bdcd7902e705c618b33906
Pim <pim.visser@amsterdam.nl>
2022-02-05 16:28:39 +0100

    Add mmApiSaveService

--- bdm/src/services/service_mm_api.js
+++ bdm/src/services/service_mm_api.js
@@ -1,2 +1,13 @@
-const mmApiBaseUrl = `${process.env.REACT_APP_KJK_API_ORIGIN}/api/mm`;
-export const mmApiService = url => fetch(`${mmApiBaseUrl}${url}`).then(r => r.json());
+const MM_API_BASE_URL = `${process.env.REACT_APP_KJK_API_ORIGIN}/api/mm`;
+
+export const mmApiService = url => fetch(`${MM_API_BASE_URL}${url}`).then(r => r.json());
+
+export const mmApiSaveService = (url, data) => {
+    fetch(`${MM_API_BASE_URL}${url}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }).then(response => response.json());
+};
