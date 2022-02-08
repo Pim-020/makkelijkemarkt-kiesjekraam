a7dac92d04a5664ecefe674261e1d3c32ffae6a2
Pim <pim.visser@amsterdam.nl>
2022-02-08 10:50:18 +0100

    KJK index express.json() middleware

--- src/index.ts
+++ src/index.ts
@@ -132,6 +132,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 // Static files that are public (robots.txt, favicon.ico)
