a3be5185d93a688cd8014e2c188588b2df5c533d
Pim <pim.visser@amsterdam.nl>
2022-02-10 12:34:39 +0100

    BDM add error handling to mm api fetch calls

--- bdm/src/services/service_mm_api.js
+++ bdm/src/services/service_mm_api.js
@@ -1,13 +1,29 @@
 const MM_API_BASE_URL = process.env.REACT_APP_KJK_API_ORIGIN || ''
 
-export const mmApiService = url => fetch(`${MM_API_BASE_URL}${url}`).then(r => r.json());
+export const mmApiService = async (url) => {
+    const response = await fetch(`${MM_API_BASE_URL}${url}`)
+    if (!response.ok) {
+        console.log(response)
+        const customError = new Error(response.statusText)
+        customError.status = response.status
+        throw customError;
+    }
+    return response.json()
+}
 
-export const mmApiSaveService = (url, data) => {
-    fetch(`${MM_API_BASE_URL}${url}`, {
+export const mmApiSaveService = async (url, data) => {
+    const response = await fetch(`${MM_API_BASE_URL}${url}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
-    }).then(response => response.json());
+    })
+    if (!response.ok) {
+        console.log(response)
+        const customError = new Error(response.statusText)
+        customError.status = response.status
+        throw customError;
+    }
+    return response.json()
 };
