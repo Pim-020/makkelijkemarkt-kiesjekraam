da281d6f9d727b2385ddab1c75c394b016f732f2
Tim Fierens <t.fierens@amsterdam.nl>
2022-02-18 14:15:54 +0100

    add plaatsvoorkeur migration script

---
+++ src/migrate-plaatsvoorkeur.ts
@@ -0,0 +1,34 @@
+import { updatePlaatsvoorkeur } from './makkelijkemarkt-api';
+import { plaatsvoorkeur } from './model/index';
+import { Plaatsvoorkeur } from 'model/plaatsvoorkeur.model';
+
+let plaatsvoorkeurenByKoopmanAndMarkt = [];
+
+plaatsvoorkeur.findAll<Plaatsvoorkeur>({
+    where: {},
+    raw: true,
+}).then( plaatsvoorkeuren => {
+    plaatsvoorkeuren.forEach( p => {
+        if(!plaatsvoorkeurenByKoopmanAndMarkt[p.erkenningsNummer]){
+            plaatsvoorkeurenByKoopmanAndMarkt[p.erkenningsNummer] = [];
+        }
+        if(!plaatsvoorkeurenByKoopmanAndMarkt[p.erkenningsNummer][p.marktId]){
+            plaatsvoorkeurenByKoopmanAndMarkt[p.erkenningsNummer][p.marktId] = [];
+        }
+        plaatsvoorkeurenByKoopmanAndMarkt[p.erkenningsNummer][p.marktId].push(p);
+    })
+    plaatsvoorkeurenByKoopmanAndMarkt.forEach( koopman => {
+        koopman.forEach( koopmanAndMarkt => {
+            console.log(koopmanAndMarkt);
+            updatePlaatsvoorkeur(koopmanAndMarkt)
+            .then( () => {
+                plaatsvoorkeur.destroy({
+                    where: {
+                        erkenningsNummer: koopmanAndMarkt[0].erkenningsNummer,
+                        marktId: koopmanAndMarkt[0].marktId,
+                    },
+                });
+            }).catch(() => console.log(`${koopmanAndMarkt[0].erkenningsNummer} with marktId ${koopmanAndMarkt[0].marktId} failed`));
+        });
+    });
+});
