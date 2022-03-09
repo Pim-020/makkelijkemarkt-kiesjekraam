a7ed29c26278824b17aa9d4167efd494b1d2d05e
Pim <pim.visser@amsterdam.nl>
2022-03-09 09:17:57 +0100

    Extend styled component for centering

--- bdm/src/components/CenterStage.styled.tsx
+++ bdm/src/components/CenterStage.styled.tsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 
-export default styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`
+export default styled.div<{ hor?: boolean; ver?: boolean; padding?: string }>(
+  ({ hor = true, ver = true, padding = '' }) => {
+    const y = ver ? '50%' : '0'
+    const x = hor ? '50%' : '0'
+    return `
+      position: absolute;
+      top: ${y};
+      left: ${x};
+      transform: translate(-${x}, -${y});
+      ${padding ? `padding: ${padding}` : ''}
+    `
+  }
+)
