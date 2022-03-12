2d63edaa25603992115b3687320c5beb8ae1f3ee
Pim <pim.visser@amsterdam.nl>
2022-03-12 17:15:02 +0100

    CSS and BEM

--- src/views/components/PullDownMenu.tsx
+++ src/views/components/PullDownMenu.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
+type Option = {
+  name: string,
+  destination: string,
+}
+
 type Props = {
-  options: {name: string, destination: string}[],
+  options: Option[],
   children: JSX.Element,
 }
 
-const PullDownMenu: (props: Props) => JSX.Element = (props) => {
-  const options = props.options.map((option:any) => <a href={option.destination}>{option.name}</a>)
+const PullDownMenu = (props: Props) => {
+  const options = props.options.map((option:Option) =>
+    <a className="pull-down-menu__option" href={option.destination}>{option.name}</a>)
   return (
-    <div className="dropdown" >
-    <button className="dropbtn">
+    <div className="pull-down-menu">
+    <button className="pull-down-menu__header">
       <span>
         {props.children}
       </span>
       </button>
-    <div className="dropdown-content">
+    <div className="pull-down-menu__content">
       {options}
     </div>
   </div>
