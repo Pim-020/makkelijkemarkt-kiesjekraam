56b3bff0d6fcd29ae55133bb84ce8766ec5b1d6e
Tim Fierens <t.fierens@amsterdam.nl>
2022-03-15 17:36:26 +0100

    revert changes to mailer uri

--- src/mail.js
+++ src/mail.js
@@ -5,7 +5,18 @@ const { URL } = require('url');
 
 requireEnv('MAILER_URL');
 
-const transport = nodemailer.createTransport(new URL(process.env.MAILER_URL));
+const { username, password, hostname, port, searchParams } = new URL(process.env.MAILER_URL);
+
+const transport = nodemailer.createTransport(
+    {
+        host: hostname,
+        port,
+        auth: {
+            user: username || searchParams.get('username'),
+            pass: password || searchParams.get('password'),
+        },
+    },
+);
 
 const mail = options => {
     if (options.react) {
