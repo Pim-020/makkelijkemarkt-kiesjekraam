79cbd63d94c751fd2145216e869038001e6b3b4c
Tim Fierens <t.fierens@amsterdam.nl>
2022-03-15 14:05:16 +0100

    remove nodemailer-sendgrid-transport

--- src/mail.js
+++ src/mail.js
@@ -1,33 +1,11 @@
 const ReactDOMServer = require('react-dom/server');
 const nodemailer = require('nodemailer');
-const sgTransport = require('nodemailer-sendgrid-transport');
 const { requireEnv } = require('./util.ts');
 const { URL } = require('url');
 
 requireEnv('MAILER_URL');
 
-const { username, password, hostname, port, searchParams } = new URL(process.env.MAILER_URL);
-
-/*
- * Use SMTP as default transport.
- * When sending mail via SendGrid, use their optimized Web API.
- */
-const transport = nodemailer.createTransport(
-    hostname === 'smtp.sendgrid.net' && username === 'apikey'
-        ? sgTransport({
-              auth: {
-                  api_key: password || searchParams.get('password'), // eslint-disable-line camelcase
-              },
-          })
-        : {
-              host: hostname,
-              port,
-              auth: {
-                  user: username || searchParams.get('username'),
-                  pass: password || searchParams.get('password'),
-              },
-          },
-);
+const transport = nodemailer.createTransport(new URL(process.env.MAILER_URL));
 
 const mail = options => {
     if (options.react) {
